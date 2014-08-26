ba22a1d3706ecbc92e6f9243dff3cd9e488850f2
avanheest <ajv218@lehigh.edu>
2014-08-26 21:32:11 +0000

    Updated homework

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,13 @@
+public class WelcomeClass{
+    public static void main(String[] args){
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-A--J--V--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("Last spring I visited England and saw Van Gogh's 'Sunflowers' with my dad.");
+    }
+}
