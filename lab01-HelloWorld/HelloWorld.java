47639ecc2ca02e920d4525500a9b28bceed57730
avanheest <ajv218@lehigh.edu>
2014-08-26 19:44:28 +0000

    Updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+// define class
+public class HelloWorld {
+    // main method
+    public static void main(String[] args) {
+        //print line commands
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test
+    }
+}
